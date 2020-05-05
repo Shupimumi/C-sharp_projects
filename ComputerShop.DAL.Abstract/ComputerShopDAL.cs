17ce7d30be21eaf6d9918a2ff0b5747d961c976f
shupimumi <shupimumi@gmail.com>
2020-05-05 23:37:41 +0400

    Работа с DAL

---
+++ ComputerShop.DAL.Abstract/ComputerShopDAL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ComputerShop.Entities;
+
+
+namespace ComputerShop.DAL.Abstract
+{
+	interface ComputerShopDAL
+	{
+
+		bool Add (ComputerShopDTO data);
+
+	}
+}
---
+++ ComputerShop/ComputerShop.DTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ComputerShop.Entities
+{
+		//DTO - Data transfer object (Предназначена для транспортировки данных в базу данных.)	
+		//+ в том, что не надо накладывать ограничения на инкапсуляцию
+		public class ComputerShopDTO 
+		{
+
+			public Guid Id { get; set; }
+			public DateTime CreatedData { get; set; }
+			public string Text { get; set; }
+		
+		}
+}
